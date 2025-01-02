efb7cb6d5b6f532d2699a532fea29c15c648457d
Pedro Henrique De Jesus Barbosa <pede-jes@c2r2p1.42sp.org.br>
2025-01-01 22:27:14 -0300

    numbers functions ready

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pede-jes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 14:37:29 by pede-jes          #+#    #+#             */
-/*   Updated: 2025/01/01 22:03:18 by pede-jes         ###   ########.fr       */
+/*   Updated: 2025/01/01 22:21:01 by pede-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int ft_putpointer(unsigned long ptr);
 int ft_putnbr(unsigned long long n);
 int ft_puthex(unsigned long n);
 int ft_puthexmax(unsigned long n);
+int ft_putnumber(int n);
 
 #endif
